kevinswiber/languageserver-go

commit 8d228ec1cb8bda1883e2ce163825e11050614a29

internal/lsp: run analyses on changed files before full diagnostics
Without this, analysis diagnostics have to wait until the entire
workspace is analyzed, which has a noticeable latency.

Refactored diagnosePkg out of diagnose to reuse code, and cleaned up
the showMsg and seen file logic a little bit.

Fixes golang/go#42499

Change-Id: I51314dab156dce87dc19eba997c53d2aea428b6c
Reviewed-on: https://go-review.googlesource.com/c/tools/+/269197
Trust: Rebecca Stambler <rstambler@golang.org>
Run-TryBot: Rebecca Stambler <rstambler@golang.org>
gopls-CI: kokoro <noreply+kokoro@google.com>
TryBot-Result: Go Bot <gobot@golang.org>
Reviewed-by: Robert Findley <rfindley@google.com>

lsp/diagnostics.go
@@ -68,10 +68,13 @@ func hashDiagnostics(diags ...*source.Diagnostic) string {
 func (s *Server) diagnoseDetached(snapshot source.Snapshot) {
 	ctx := snapshot.View().BackgroundContext()
 	ctx = xcontext.Detach(ctx)
-	shows := s.diagnose(ctx, snapshot, false)
-	if shows != nil {
+	showWarning := s.diagnose(ctx, snapshot, false)
+	if showWarning {
 		// If a view has been created or the configuration changed, warn the user.
-		s.client.ShowMessage(ctx, shows)
+		s.client.ShowMessage(ctx, &protocol.ShowMessageParams{
+			Type:    protocol.Warning,
+			Message: `You are neither in a module nor in your GOPATH. If you are using modules, please open your editor to a directory in your module. If you believe this warning is incorrect, please file an issue: https://github.com/golang/go/issues/new.`,
+		})
 	}
 	s.publishDiagnostics(ctx, true, snapshot)
 }
@@ -83,10 +86,11 @@ func (s *Server) diagnoseSnapshot(snapshot source.Snapshot, changedURIs []span.U
 	if delay > 0 {
 		// Experimental 2-phase diagnostics.
 		//
-		// The first phase just parses and checks packages that have been affected
-		// by file modifications (no analysis).
+		// The first phase just parses and checks packages that have been
+		// affected by file modifications (no analysis).
 		//
-		// The second phase does everything, and is debounced by the configured delay.
+		// The second phase does everything, and is debounced by the configured
+		// delay.
 		s.diagnoseChangedFiles(ctx, snapshot, changedURIs, onDisk)
 		s.publishDiagnostics(ctx, false, snapshot)
 		s.debouncer.debounce(snapshot.View().Name(), snapshot.ID(), delay, func() {
@@ -122,24 +126,29 @@ func (s *Server) diagnoseChangedFiles(ctx context.Context, snapshot source.Snaps
 			packages[pkg] = struct{}{}
 		}
 	}
+	var wg sync.WaitGroup
 	for pkg := range packages {
-		typeCheckResults := source.GetTypeCheckDiagnostics(ctx, snapshot, pkg)
-		for uri, diags := range typeCheckResults.Diagnostics {
-			s.storeDiagnostics(snapshot, uri, typeCheckSource, diags)
-		}
+		wg.Add(1)
+
+		go func(pkg source.Package) {
+			defer wg.Done()
+
+			_ = s.diagnosePkg(ctx, snapshot, pkg, true)
+		}(pkg)
 	}
+	wg.Wait()
 }
 
 // diagnose is a helper function for running diagnostics with a given context.
 // Do not call it directly.
-func (s *Server) diagnose(ctx context.Context, snapshot source.Snapshot, alwaysAnalyze bool) (_ *protocol.ShowMessageParams) {
+func (s *Server) diagnose(ctx context.Context, snapshot source.Snapshot, alwaysAnalyze bool) bool {
 	ctx, done := event.Start(ctx, "Server.diagnose")
 	defer done()
 
 	// Wait for a free diagnostics slot.
 	select {
 	case <-ctx.Done():
-		return nil
+		return false
 	case s.diagnosticsSema <- struct{}{}:
 	}
 	defer func() {
@@ -149,7 +158,7 @@ func (s *Server) diagnose(ctx context.Context, snapshot source.Snapshot, alwaysA
 	// First, diagnose the go.mod file.
 	modReports, modErr := mod.Diagnostics(ctx, snapshot)
 	if ctx.Err() != nil {
-		return nil
+		return false
 	}
 	if modErr != nil {
 		event.Error(ctx, "warning: diagnose go.mod", modErr, tag.Directory.Of(snapshot.View().Folder().Filename()), tag.Snapshot.Of(snapshot.ID()))
@@ -165,7 +174,7 @@ func (s *Server) diagnose(ctx context.Context, snapshot source.Snapshot, alwaysA
 	// Diagnose all of the packages in the workspace.
 	wsPkgs, err := snapshot.WorkspacePackages(ctx)
 	if s.shouldIgnoreError(ctx, snapshot, err) {
-		return nil
+		return false
 	}
 	// Show the error as a progress error report so that it appears in the
 	// status bar. If a client doesn't support progress reports, the error
@@ -179,80 +188,30 @@ func (s *Server) diagnose(ctx context.Context, snapshot source.Snapshot, alwaysA
 			s.storeErrorDiagnostics(ctx, snapshot, typeCheckSource, errList)
 		}
 		event.Error(ctx, "errors diagnosing workspace", err, tag.Snapshot.Of(snapshot.ID()), tag.Directory.Of(snapshot.View().Folder()))
-		return nil
+		return false
 	}
 
 	var (
-		showMsgMu sync.Mutex
-		showMsg   *protocol.ShowMessageParams
-		wg        sync.WaitGroup
-		seenMu    sync.Mutex
-		seen      = map[span.URI]bool{}
+		wg              sync.WaitGroup
+		shouldShowMsgMu sync.Mutex
+		shouldShowMsg   bool
+		seen            = map[span.URI]struct{}{}
 	)
 	for _, pkg := range wsPkgs {
 		wg.Add(1)
+
+		for _, pgf := range pkg.CompiledGoFiles() {
+			seen[pgf.URI] = struct{}{}
+		}
+
 		go func(pkg source.Package) {
 			defer wg.Done()
 
-			includeAnalysis := alwaysAnalyze // only run analyses for packages with open files
-			var gcDetailsDir span.URI        // find the package's optimization details, if available
-			for _, pgf := range pkg.CompiledGoFiles() {
-				seenMu.Lock()
-				seen[pgf.URI] = true
-				seenMu.Unlock()
-				if snapshot.IsOpen(pgf.URI) {
-					includeAnalysis = true
-				}
-				if gcDetailsDir == "" {
-					dirURI := span.URIFromPath(filepath.Dir(pgf.URI.Filename()))
-					s.gcOptimizationDetailsMu.Lock()
-					_, ok := s.gcOptimizationDetails[dirURI]
-					s.gcOptimizationDetailsMu.Unlock()
-					if ok {
-						gcDetailsDir = dirURI
-					}
-				}
-			}
-
-			if shouldWarn(snapshot, pkg) {
-				showMsgMu.Lock()
-				showMsg = &protocol.ShowMessageParams{
-					Type:    protocol.Warning,
-					Message: `You are neither in a module nor in your GOPATH. If you are using modules, please open your editor to a directory in your module. If you believe this warning is incorrect, please file an issue: https://github.com/golang/go/issues/new.`,
-				}
-				showMsgMu.Unlock()
-			}
-
-			typeCheckResults := source.GetTypeCheckDiagnostics(ctx, snapshot, pkg)
-			for uri, diags := range typeCheckResults.Diagnostics {
-				s.storeDiagnostics(snapshot, uri, typeCheckSource, diags)
-			}
-			if includeAnalysis && !typeCheckResults.HasParseOrListErrors {
-				reports, err := source.Analyze(ctx, snapshot, pkg, typeCheckResults)
-				if err != nil {
-					event.Error(ctx, "warning: diagnose package", err, tag.Snapshot.Of(snapshot.ID()), tag.Package.Of(pkg.ID()))
-					return
-				}
-				for uri, diags := range reports {
-					s.storeDiagnostics(snapshot, uri, analysisSource, diags)
-				}
-			}
-			// If gc optimization details are available, add them to the
-			// diagnostic reports.
-			if gcDetailsDir != "" {
-				gcReports, err := source.GCOptimizationDetails(ctx, snapshot, gcDetailsDir)
-				if err != nil {
-					event.Error(ctx, "warning: gc details", err, tag.Snapshot.Of(snapshot.ID()), tag.Package.Of(pkg.ID()))
-				}
-				for id, diags := range gcReports {
-					fh := snapshot.FindFile(id.URI)
-					// Don't publish gc details for unsaved buffers, since the underlying
-					// logic operates on the file on disk.
-					if fh == nil || !fh.Saved() {
-						continue
-					}
-					s.storeDiagnostics(snapshot, id.URI, gcDetailsSource, diags)
-				}
+			show := s.diagnosePkg(ctx, snapshot, pkg, alwaysAnalyze)
+			if show {
+				shouldShowMsgMu.Lock()
+				shouldShowMsg = true
+				shouldShowMsgMu.Unlock()
 			}
 		}(pkg)
 	}
@@ -261,7 +220,7 @@ func (s *Server) diagnose(ctx context.Context, snapshot source.Snapshot, alwaysA
 	// the workspace). Otherwise, add a diagnostic to the file.
 	if len(wsPkgs) > 0 {
 		for _, o := range s.session.Overlays() {
-			if seen[o.URI()] {
+			if _, ok := seen[o.URI()]; ok {
 				continue
 			}
 			diagnostic := s.checkForOrphanedFile(ctx, snapshot, o)
@@ -271,7 +230,59 @@ func (s *Server) diagnose(ctx context.Context, snapshot source.Snapshot, alwaysA
 			s.storeDiagnostics(snapshot, o.URI(), orphanedSource, []*source.Diagnostic{diagnostic})
 		}
 	}
-	return showMsg
+	return shouldShowMsg
+}
+
+func (s *Server) diagnosePkg(ctx context.Context, snapshot source.Snapshot, pkg source.Package, alwaysAnalyze bool) bool {
+	includeAnalysis := alwaysAnalyze // only run analyses for packages with open files
+	var gcDetailsDir span.URI        // find the package's optimization details, if available
+	for _, pgf := range pkg.CompiledGoFiles() {
+		if snapshot.IsOpen(pgf.URI) {
+			includeAnalysis = true
+		}
+		if gcDetailsDir == "" {
+			dirURI := span.URIFromPath(filepath.Dir(pgf.URI.Filename()))
+			s.gcOptimizationDetailsMu.Lock()
+			_, ok := s.gcOptimizationDetails[dirURI]
+			s.gcOptimizationDetailsMu.Unlock()
+			if ok {
+				gcDetailsDir = dirURI
+			}
+		}
+	}
+
+	typeCheckResults := source.GetTypeCheckDiagnostics(ctx, snapshot, pkg)
+	for uri, diags := range typeCheckResults.Diagnostics {
+		s.storeDiagnostics(snapshot, uri, typeCheckSource, diags)
+	}
+	if includeAnalysis && !typeCheckResults.HasParseOrListErrors {
+		reports, err := source.Analyze(ctx, snapshot, pkg, typeCheckResults)
+		if err != nil {
+			event.Error(ctx, "warning: diagnose package", err, tag.Snapshot.Of(snapshot.ID()), tag.Package.Of(pkg.ID()))
+			return false
+		}
+		for uri, diags := range reports {
+			s.storeDiagnostics(snapshot, uri, analysisSource, diags)
+		}
+	}
+	// If gc optimization details are available, add them to the
+	// diagnostic reports.
+	if gcDetailsDir != "" {
+		gcReports, err := source.GCOptimizationDetails(ctx, snapshot, gcDetailsDir)
+		if err != nil {
+			event.Error(ctx, "warning: gc details", err, tag.Snapshot.Of(snapshot.ID()), tag.Package.Of(pkg.ID()))
+		}
+		for id, diags := range gcReports {
+			fh := snapshot.FindFile(id.URI)
+			// Don't publish gc details for unsaved buffers, since the underlying
+			// logic operates on the file on disk.
+			if fh == nil || !fh.Saved() {
+				continue
+			}
+			s.storeDiagnostics(snapshot, id.URI, gcDetailsSource, diags)
+		}
+	}
+	return shouldWarn(snapshot, pkg)
 }
 
 // storeDiagnostics stores results from a single diagnostic source. If merge is

commit 33a3007fc61634bdf6810b2e39fe2602a8c3caf1

internal/lsp/source: default experimentalDiagnosticsDelay to 250ms
We've been aiming to enable diagnostic debouncing by default, and now
that CL 269197 is merged, I think it's time to do that.

I know that the experimental setting had it at 200ms, but the 250ms was
specified in golang/go#42693, so I'm not sure which is preferable.

Also, fix a bug I introduced in CL 269197, which set alwaysAnalyze to
true for diagnoseChangedFiles. This should only be used for testing.

Add a requirement for 1.13 to one test that relies on good overlay
support (loading the file directly produced a different response from
the full workspace load).

Fixes golang/go#42693

Change-Id: I49b764a8d473a66a01ef0f2c6f4cbe2d512fd64e
Reviewed-on: https://go-review.googlesource.com/c/tools/+/274445
Trust: Rebecca Stambler <rstambler@golang.org>
Run-TryBot: Rebecca Stambler <rstambler@golang.org>
gopls-CI: kokoro <noreply+kokoro@google.com>
TryBot-Result: Go Bot <gobot@golang.org>
Reviewed-by: Robert Findley <rfindley@google.com>

lsp/diagnostics.go
@@ -115,6 +115,11 @@ func (s *Server) diagnoseChangedFiles(ctx context.Context, snapshot source.Snaps
 		if onDisk && !snapshot.IsOpen(uri) {
 			continue
 		}
+		// If the file is not known to the snapshot (e.g., if it was deleted),
+		// don't diagnose it.
+		if snapshot.FindFile(uri) == nil {
+			continue
+		}
 		pkgs, err := snapshot.PackagesForFile(ctx, uri, source.TypecheckWorkspace)
 		if err != nil {
 			// TODO (findleyr): we should probably do something with the error here,
@@ -133,15 +138,15 @@ func (s *Server) diagnoseChangedFiles(ctx context.Context, snapshot source.Snaps
 		go func(pkg source.Package) {
 			defer wg.Done()
 
-			_ = s.diagnosePkg(ctx, snapshot, pkg, true)
+			_ = s.diagnosePkg(ctx, snapshot, pkg, false)
 		}(pkg)
 	}
 	wg.Wait()
 }
 
 // diagnose is a helper function for running diagnostics with a given context.
-// Do not call it directly.
-func (s *Server) diagnose(ctx context.Context, snapshot source.Snapshot, alwaysAnalyze bool) bool {
+// Do not call it directly. forceAnalysis is only true for testing purposes.
+func (s *Server) diagnose(ctx context.Context, snapshot source.Snapshot, forceAnalysis bool) bool {
 	ctx, done := event.Start(ctx, "Server.diagnose")
 	defer done()
 
@@ -204,7 +209,7 @@ func (s *Server) diagnose(ctx context.Context, snapshot source.Snapshot, alwaysA
 		go func(pkg source.Package) {
 			defer wg.Done()
 
-			show := s.diagnosePkg(ctx, snapshot, pkg, alwaysAnalyze)
+			show := s.diagnosePkg(ctx, snapshot, pkg, forceAnalysis)
 			if show {
 				shouldShowMsgMu.Lock()
 				shouldShowMsg = true

lsp/source/api_json.go
@@ -61,7 +61,7 @@ var GeneratedAPIJSON = &APIJSON{
 				Type:       "time.Duration",
 				Doc:        "experimentalDiagnosticsDelay controls the amount of time that gopls waits\nafter the most recent file modification before computing deep diagnostics.\nSimple diagnostics (parsing and type-checking) are always run immediately\non recently modified packages.\n\nThis option must be set to a valid duration string, for example `\"250ms\"`.\n",
 				EnumValues: nil,
-				Default:    "\"0s\"",
+				Default:    "\"250ms\"",
 			},
 			{
 				Name:       "experimentalPackageCacheKey",

lsp/source/options.go
@@ -120,8 +120,9 @@ func DefaultOptions() *Options {
 				CompletionBudget: 100 * time.Millisecond,
 			},
 			ExperimentalOptions: ExperimentalOptions{
-				ExpandWorkspaceToModule:     true,
-				ExperimentalPackageCacheKey: true,
+				ExpandWorkspaceToModule:      true,
+				ExperimentalPackageCacheKey:  true,
+				ExperimentalDiagnosticsDelay: 250 * time.Millisecond,
 			},
 			InternalOptions: InternalOptions{
 				LiteralCompletions:      true,
@@ -625,11 +626,10 @@ func (o *Options) AddStaticcheckAnalyzer(a *analysis.Analyzer) {
 }
 
 // enableAllExperiments turns on all of the experimental "off-by-default"
-// features offered by gopls.
-// Any experimental features specified in maps should be enabled in
-// enableAllExperimentMaps.
+// features offered by gopls. Any experimental features specified in maps
+// should be enabled in enableAllExperimentMaps.
 func (o *Options) enableAllExperiments() {
-	o.ExperimentalDiagnosticsDelay = 200 * time.Millisecond
+	// There are currently no experimental features in development.
 }
 
 func (o *Options) enableAllExperimentMaps() {